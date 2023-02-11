9a96bc1ce2081d10d2024a2e7c24d5025cf9e82e
Anatoliy Pivovarskiy <a.pivovarskii91@gmail.com>
2023-02-11 15:47:35 +0200

    slider classes

--- assets/js/carousel-swipe.js
+++ assets/js/carousel-swipe.js
@@ -1,20 +1,32 @@
-function SwipeCarousel() {
-  Carousel.apply(this, arguments);
+import Carousel from './carousel.js';
+
+class SwipeCarousel extends Carousel{
+  constructor(...args){
+    super(...args);
+    this.slidesContainer = this.slideItems[0].parentElement;
+    }
+
+    _initListeners () {
+  super._initListeners();
+  this.container.addEventListener('touchstart', this._swipeStart.bind(this));
+  this.slidesContainer.addEventListener('mousedown', this._swipeStart.bind(this));
+  this.container.addEventListener('touchend', this._swipeEnd.bind(this));
+  this.slidesContainer.addEventListener('mouseup', this._swipeEnd.bind(this));
 }
 
-SwipeCarousel.prototype = Object.create(Carousel.prototype);
-SwipeCarousel.prototype.constructor = SwipeCarousel;
-
-SwipeCarousel.prototype._swipeStart = function(e) {
-  this.startPosX = e instanceof MouseEvent
-    ? e.pageX
-    : e.changedTouches[0].pageX
-};
-
-SwipeCarousel.prototype._swipeEnd = function(e) {
-  this.endPosX = e instanceof MouseEvent
-    ? e.pageX
-    : e.changedTouches[0].pageX
-  if (this.endPosX - this.startPosX > -100) this.prev();
-  if (this.endPosX - this.startPosX < 100) this.next();
+  _swipeStart (e) {
+    this.startPosX = e instanceof MouseEvent
+      ? e.pageX
+      : e.changedTouches[0].pageX
+  }
+  
+  _swipeEnd (e) {
+    this.endPosX = e instanceof MouseEvent
+      ? e.pageX
+      : e.changedTouches[0].pageX
+    if (this.endPosX - this.startPosX > -100) this.prev();
+    if (this.endPosX - this.startPosX < 100) this.next();
+  }
 }
+
+export default  SwipeCarousel;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
+import SwipeCarousel from './carousel-swipe.js';
 
-const carousel = new SwipeCarousel();
+const carousel = new SwipeCarousel({
+  // containerID:'#carousel',
+  // slideID: 'item', 
+  interval: 2000,
+  isPlaying:false
+});
 
 carousel.initApp();
